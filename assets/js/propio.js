491d506c1eae994bd017cb5bf1b9f45e91eef0ad
tecsya02 <tecsya02@sbif.cl>
2019-05-23 18:19:53 -0400

    Función para ingresar datos de factura en progreso

--- assets/js/propio.js
+++ assets/js/propio.js
@@ -1,4 +1,5 @@
 //Definir variables
+const listadoFacturas = document.getElementById('listado-facturas');
 const numeroFactura = document.getElementById('valor-factura');
 const calcular = document.getElementById('calcular');
 const ingresarFactura = document.getElementById('ingresar-factura');
@@ -8,11 +9,14 @@ const reiniciar = document.getElementById('reiniciar');
 // const valorTotal = document.getElementById('valor-total');
 
 
-//Definir event listeners
+//Se definen los event listeners por cada elemento del formulario
 calcular.addEventListener('click', calcularFactura);
 ingresarFactura.addEventListener('click', ingresarDatosFactura);
 reiniciar.addEventListener('click', reiniciarFormulario);
 
+
+//Se va a calcular el IVA y el total de la factura a partir del valor neto, y lo mostrará
+//en los casilleros correspondientes antes de ingresarlos al Record
 function calcularFactura() {
     const valorNeto = document.getElementById('valor-neto').value;  
     let resultadoIva = parseInt(valorNeto) * 0.19;
@@ -23,9 +27,43 @@ function calcularFactura() {
 }
 
 function ingresarDatosFactura(){
-    console.log('Ingreso de datos funciona');
+    // alert('Ingreso de datos funciona');
+    const valorNeto = document.getElementById('valor-neto').value;
+    const resultadoIva = document.getElementById('iva').value;
+    const resultadoTotal = document.getElementById('valor-total').value;
+    botonBorrar = document.createElement('a');
+    botonBorrar.classList.add('delete','is-medium');
+    // const trNumeroFactura = document.createElement('tr');
+    // const th = document.createElement('th');
+    // const td = document.createElement('td');
+    // // const numero = document.createTextNode('Prueba 1');
+    // th.innerText = numeroFactura;
+    
+    // const neto = document.createTextNode('Prueba 2');
+    // const ivaT = document.createTextNode('Prueba 3');
+    // th.appendChild(numero);
+    // td.appendChild(neto);
+    // td.appendChild(ivaT);
+    // tr.appendChild(th);
+    // tr.appendChild(td);
+
+    // tr.innerHTML = numeroFactura + '</td><td>' + valorNeto + '</td><td>' + resultadoIva + '</td><td>' + resultadoTotal + '</td>';
+    tr.appendChild(botonBorrar);
+    listadoFacturas.appendChild(tr);
+    // const li = document.createElement('li');
+    // li.innerText = "Prueba";
+    // li.appendChild(botonBorrar);
+    // listadoFacturas.appendChild(li);
+
 }
 
 function reiniciarFormulario(){
-    console.log('Reinicio del formulario funciona');
+    alert('Reinicio del formulario funciona');
+}
+
+
+function mostrarTablaFacturas(){
+    const valorNeto = document.getElementById('valor-neto').value;
+    const resultadoIva = document.getElementById('iva').value;
+    const resultadoTotal = document.getElementById('valor-total').value;
 }
